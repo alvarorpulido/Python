6a0f054c386a52bc491a82c83a10311be2a76a06
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-09-30 14:35:38 +0200

    ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+x = 112 
+y = 75  
+z = int(input("Número de payasos vendidos: "))
+n = int(input("Número de muñecas vendidas: "))
+peso_total = (z * x) + (n * y)
+print(f"El peso total del paquete es: {peso_total} gramos")
