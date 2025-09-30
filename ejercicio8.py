e79ceba820d9426444b5e12116107a97b9d152f7
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-09-30 14:22:59 +0200

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+n = int(input("Introduce un número entero: "))
+m = int(input("Introduce otro número entero: "))
+c = n // m
+r = n % m
+print(f"El cociente entre {n} y {m} es {c} y el resto es {r}")
