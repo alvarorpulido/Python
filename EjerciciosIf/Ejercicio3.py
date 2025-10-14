9ea80d51ab18c9543daa3f29ce170d5453b864b6
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-10-14 14:37:08 +0200

    Ejercicio3 If

---
+++ EjerciciosIf/Ejercicio3.py
@@ -0,0 +1,9 @@
+n1 = int(input("Dame el dividendo: "))
+n2 = int(input("Dame el divisor: "))
+
+if n2 == 0:
+    print ("Syntax error")
+else:
+    r = n1 / n2
+    print (f"El resultado es: {r}")
+    
