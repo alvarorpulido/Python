9645d9848a5ffe2ba162f5df9f175a5fbae2ec92
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-10-14 14:43:07 +0200

    Ejercicio5 If

---
+++ EjerciciosIf/Ejercicio5.py
@@ -0,0 +1,6 @@
+edad = float(input("Introduce tu edad: "))
+ingr = float(input ("Introduce tu salario mensual: "))
+if edad > 16 and ingr >= 1000:
+    print ("Tienes que tributar")
+else:
+    print ("No tienes que tributar")
