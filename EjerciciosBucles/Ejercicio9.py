f3490e655e7316eaf6152713df10372430787d82
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-11-05 10:06:29 +0100

    Ejercicio9 Bucles

---
+++ EjerciciosBucles/Ejercicio9.py
@@ -0,0 +1,7 @@
+contraseña = "User123*"
+entrada = ""
+while entrada != contraseña:
+    entrada = input("Introduce la contraseña: ")
+    if entrada != contraseña:
+        print("Contraseña incorrecta")
+print("Contraseña correcta")
