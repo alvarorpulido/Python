79b2613f368eba5f27f847549fd9435736ed6fc7
Álvaro Rodríguez 2ºASIR B <arodpul3005@ieszaidinvergeles.org>
2025-12-02 14:05:20 +0100

    Ejercicio3 Listas

---
+++ EjerciciosListas/Ejercicio3.py
@@ -0,0 +1,4 @@
+list = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+for i in range(len(list)):
+    x = input(f"¿Qué has sacado en " + list[i] + "?: ")
+    print(f"En la asignatura " + list[i] + " has sacado un " + x) 
